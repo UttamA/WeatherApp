af2ad1efe1e1529d6c39198e013a6994431b93df
Uttam <uttamagarwal.slg@gmail.com>
2016-05-15 13:47:42 +0530

    Description : Testing email
    Documentation Notes: None
    QA Notes: None
    Testing Done: manual

--- app/src/main/java/com/satya/weather/ui/model/Clouds.java
+++ app/src/main/java/com/satya/weather/ui/model/Clouds.java
@@ -4,7 +4,7 @@ import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 
-public class Clouds {
+public class Clouds  {
 
     @SerializedName("all")
     @Expose
